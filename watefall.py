222a21f82153a82c114eac26974772dddac90d3b
zperkowski <ZdzislawPerkowski@outlook.com>
2018-12-09 15:44:56 +0100

    Higher resolution

--- watefall.py
+++ watefall.py
@@ -4,7 +4,7 @@ import map_generator
 if __name__ == '__main__':
     request = "https://api.open-elevation.com/api/v1/lookup?locations="
     coordinates = map_generator.build_request_string(
-        map_generator.build_list_of_coordinates(54.751923, 13.802965, 48.652599, 24.520693, 0.81))
+        map_generator.build_list_of_coordinates(54.75, 13.80, 48.65, 24.52, 0.5))
     request += coordinates
     response = map_generator.send_request(request)
     print(str(response))
